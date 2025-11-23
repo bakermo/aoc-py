a605ddaa7611598820887eada0de241639741cf3
bakermo <matthew.baker4175@gmail.com>
2025-11-23 17:31:29 -0500

    more testing

--- puzzles/2024/01/solution.py
+++ puzzles/2024/01/solution.py
@@ -6,14 +6,47 @@ def parse(input):
 
 
 def part_one(input: Sequence[str]):
-    return 0
+
+    left = []
+    right = []
+
+    for row in input:
+        split = row.split()
+        left.append(int(split[0]))
+        right.append(int(split[1]))
+
+    left.sort()
+    right.sort()
+
+    distances = []
+    for x in range(0, len(left)):
+        distance = abs(left[x] - right[x])
+        distances.append(distance)
+
+    return sum(distances)
 
 
 def part_two(input: Sequence[str]):
-    return 0
+    left = []
+    right = []
+
+    for row in input:
+        split = row.split()
+        left.append(int(split[0]))
+        right.append(int(split[1]))
+
+    similarities = []
+    for x in range(0, len(left)):
+        similarity = 0
+        for y in range(0, len(right)):
+            if left[x] == right[y]:
+                similarity += left[x]
+
+        similarities.append(similarity)
+    return sum(similarities)
 
 
-isTest = True
+isTest = False
 
 filePath = "sample.txt" if isTest else "input.txt"
 print(f"file: {filePath}")
