8ecd4cde4d7bc14940334fe13185fff39a52b163
Matt Baker <matthew.baker4175@gmail.com>
2025-11-27 21:09:24 -0500

    fun little confidence boost

---
+++ puzzles/2017/04/solution.py
@@ -0,0 +1,74 @@
+
+# import sys
+# # we need this obnoxiousness to reference the utils package in repo root
+# sys.path.append("../../../")
+# from aoc_py_utils import utils
+from collections.abc import Sequence
+
+
+def part_one(input: Sequence[str]):
+    valid_count = 0
+    for line in input:
+        tokens = line.split()
+        used = set();
+        valid = True
+        for t in tokens:
+            print(used)
+            if t not in used:
+                used.add(t)
+            else:
+                valid = False
+
+        if valid:
+            valid_count += 1
+            
+    return valid_count
+
+def sort_str(token: str) -> str:
+    cs = []
+    for c in token:
+        cs.append(c)
+
+    acc = ''
+    cs.sort()
+    for c in cs:
+        acc += c
+
+    return acc
+
+def part_two(input: Sequence[str]):
+    valid_count = 0
+    for line in input:
+        tokens = line.split()
+        used = set();
+        valid = True
+        for t in tokens:
+            # print(f't: {t}')
+            s = sort_str(t)
+            # print(f's: {s}')
+            if s not in used:
+                used.add(s)
+            else:
+                valid = False
+           
+
+        if valid:
+            valid_count += 1
+    return valid_count
+
+
+is_test = False
+
+file_path = "sample.txt" if is_test else "input.txt"
+print(f"file: {file_path}")
+
+input = []
+
+file = open(f"{file_path}")
+
+for line in file:
+    input.append(str(line.strip()))
+
+print(f"Part 1:\n\n{part_one(input)}\n\n")
+
+print(f"Part 2:\n\n{part_two(input)}\n\n")
